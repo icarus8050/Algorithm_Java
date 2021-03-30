019be5aab10170a6853a9d6e2ed9c5d402dafcef
icarus8050 <icarus8050@naver.com>
2021-03-31 00:37:56 +0900

    136. Single Number (https://leetcode.com/problems/single-number/) beauty code

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -1,23 +1,14 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 136. Single Number (https://leetcode.com/problems/single-number/)
  */
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        int ans = 0;
         for (int num : nums) {
-            if (set.contains(num)) {
-                set.remove(num);
-            } else {
-                set.add(num);
-            }
+            ans ^= num;
         }
-
-        return set.stream().mapToInt(Integer::intValue)
-                .findFirst().getAsInt();
+        return ans;
     }
 }
