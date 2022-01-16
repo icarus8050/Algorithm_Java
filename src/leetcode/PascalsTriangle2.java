8ed5605ecc1616926a93bea75935c707da9ccab9
icarus8050 <icarus8050@naver.com>
2022-01-16 10:01:54 +0900

    119. Pascals Triangle2 (https://leetcode.com/problems/pascals-triangle-ii/)

--- src/leetcode/PascalsTriangle2.java
+++ src/leetcode/PascalsTriangle2.java
@@ -24,4 +24,17 @@ public class PascalsTriangle2 {
 
         return answer.get(answer.size() - 1);
     }
+
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i <= rowIndex; i++) {
+            answer.add(1);
+            for (int j = i - 1; j > 0; j--) {
+                answer.set(j, answer.get(j - 1) + answer.get(j));
+            }
+        }
+
+        return answer;
+    }
 }
