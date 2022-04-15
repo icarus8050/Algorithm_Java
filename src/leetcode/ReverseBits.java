6c5ddf1a690c9bd9fdf885e23bdd5a9eb2b7576f
icarus8050 <icarus8050@naver.com>
2022-04-16 00:39:52 +0900

    190. Reverse Bits (https://leetcode.com/problems/reverse-bits/)

---
+++ src/leetcode/ReverseBits.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * 190. Reverse Bits (https://leetcode.com/problems/reverse-bits/)
+ */
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+}
