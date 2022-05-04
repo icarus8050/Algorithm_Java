df164cc60662ab19faefa080a2838cf0e7f8dce9
icarus8050 <icarus8050@naver.com>
2022-05-05 08:42:12 +0900

    242. Valid Anagram (https://leetcode.com/problems/valid-anagram/) (optimizing)

--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -5,19 +5,16 @@ package leetcode;
  */
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        int[] cnt = new int[26];
-        for (char ch : t.toCharArray()) {
-            cnt[ch - 97]++;
-        }
+        if (s.length() != t.length()) return false;
 
-        for (char ch : s.toCharArray()) {
-            cnt[ch - 97]--;
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[t.charAt(i) - 97]++;
+            cnt[s.charAt(i) - 97]--;
         }
 
         for (int i : cnt) {
-            if (i != 0) {
-                return false;
-            }
+            if (i != 0) return false;
         }
 
         return true;
