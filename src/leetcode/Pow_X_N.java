6d5fcc1feaed8e3b5a906f1d298c9a517f0a862b
icarus8050 <icarus8050@naver.com>
2022-05-14 09:49:13 +0900

    50. Pot(x, n) (https://leetcode.com/problems/powx-n/)

---
+++ src/leetcode/Pow_X_N.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * 50. Pot(x, n) (https://leetcode.com/problems/powx-n/)
+ */
+public class Pow_X_N {
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
