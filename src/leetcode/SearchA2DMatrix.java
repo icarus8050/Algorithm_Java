74c96f79e1d14c56d776c0c5365761536d8cd248
icarus8050 <icarus8050@naver.com>
2022-05-22 11:39:26 +0900

    74. Search a 2D Matrix (https://leetcode.com/problems/search-a-2d-matrix/) (binary search)

--- src/leetcode/SearchA2DMatrix.java
+++ src/leetcode/SearchA2DMatrix.java
@@ -4,6 +4,7 @@ package leetcode;
  * 74. Search a 2D Matrix (https://leetcode.com/problems/search-a-2d-matrix/)
  */
 public class SearchA2DMatrix {
+    /*
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = 0;
 
@@ -18,6 +19,24 @@ public class SearchA2DMatrix {
             }
         }
 
+        return false;
+    }
+    */
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] > target) {
+                j--;
+            }
+            else if (matrix[i][j] < target) {
+                i++;
+            }
+        }
         return false;
     }
 }
