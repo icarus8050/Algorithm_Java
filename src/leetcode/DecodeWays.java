9df68981832539945315fc1ce113d85271d82953
icarus8050 <icarus8050@naver.com>
2022-06-05 01:30:55 +0900

    91. Decode Ways (https://leetcode.com/problems/decode-ways/)

---
+++ src/leetcode/DecodeWays.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 91. Decode Ways (https://leetcode.com/problems/decode-ways/)
+ */
+public class DecodeWays {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
