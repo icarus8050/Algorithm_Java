b32165eb3ee93244bf4d493c978bed9d12494c30
icarus8050 <icarus8050@naver.com>
2022-10-02 17:19:24 +0900

    Missing Numbers (https://www.hackerrank.com/challenges/missing-numbers/problem)

---
+++ src/hackerrank/MissingNumbers.java
@@ -0,0 +1,63 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * Missing Numbers (https://www.hackerrank.com/challenges/missing-numbers/problem)
+ */
+public class MissingNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = missingNumbers(arr, brr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static List<Integer> missingNumbers(List<Integer> arr, List<Integer> brr) {
+        List<Integer> result = new ArrayList<>();
+
+        Map<Integer, Long> arrMap = arr.stream()
+                .collect(groupingBy(v -> v, counting()));
+        Map<Integer, Long> brrMap = brr.stream()
+                .collect(groupingBy(v -> v, counting()));
+
+        for (Map.Entry<Integer, Long> brrEntry : brrMap.entrySet()) {
+            Integer key = brrEntry.getKey();
+            Long value = brrEntry.getValue();
+
+            Long arrValue = arrMap.getOrDefault(key, 0L);
+            if (!value.equals(arrValue)) {
+                result.add(key);
+            }
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
