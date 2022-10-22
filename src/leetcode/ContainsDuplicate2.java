c8ed4b34fd884ffb13c964f02b8bd7cc08f9afe5
icarus8050 <icarus8050@naver.com>
2022-10-22 17:50:33 +0900

    217. Contains Duplicate 2 (https://leetcode.com/problems/contains-duplicate-ii/) - advanced solving

--- src/leetcode/ContainsDuplicate2.java
+++ src/leetcode/ContainsDuplicate2.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 217. Contains Duplicate 2 (https://leetcode.com/problems/contains-duplicate-ii/)
  */
@@ -11,6 +14,20 @@ public class ContainsDuplicate2 {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        int length = nums.length;
+        Set<Integer> set = new HashSet<>(length);
+        for (int i = 0; i < length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
             }
         }
         return false;
