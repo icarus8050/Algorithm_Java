4e7999022ff0536c05a7d68ff73e8a8bff712547
icarus8050 <icarus8050@naver.com>
2023-09-30 20:11:34 +0900

    222. Count Complete Tree Nodes (https://leetcode.com/problems/count-complete-tree-nodes/description/)

---
+++ src/leetcode/CountCompleteTreeNodes.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 222. Count Complete Tree Nodes (https://leetcode.com/problems/count-complete-tree-nodes/description/)
+ */
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
